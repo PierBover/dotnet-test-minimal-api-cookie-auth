31aa61874f4488e8b88d5c642e8ad9b8c786ade6
Pier Bover <pierbover11@gmail.com>
2025-01-22 14:32:21 -0600

    cookie settings and comments

--- Program.cs
+++ Program.cs
@@ -5,33 +5,44 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthentication("cookie")
 .AddCookie("cookie", options => {
+	// configure cookie settings
+	options.Cookie.Name = "api-cookie";
+	options.Cookie.MaxAge = TimeSpan.FromDays(7);
+	options.Cookie.SameSite = SameSiteMode.Strict;
+
+	// when authentication fails
 	options.Events.OnRedirectToLogin = context => {
 		context.Response.StatusCode = StatusCodes.Status401Unauthorized;
 		return Task.CompletedTask;
 	};
 
+	// when authorization fails
 	options.Events.OnRedirectToAccessDenied = context => {
 		context.Response.StatusCode = StatusCodes.Status403Forbidden;
 		return Task.CompletedTask;
 	};
+
+	// to configure cookie renewal use
+	// options.Events.CheckSlidingExpiration = context => {}
+	// more info: https://github.com/dotnet/aspnetcore/blob/main/src/Security/Authentication/Cookies/samples/CookieSample/Program.cs#L18-L30
 });
 
-builder.Services.AddAuthorization(builder => {
-	builder.AddPolicy("admin", policyBuilder => {
-		policyBuilder.RequireAuthenticatedUser()
-		.AddAuthenticationSchemes("cookie")
-		.RequireClaim("role", "admin");
-	});
+builder.Services.AddAuthorizationBuilder()
+.AddPolicy("admin", policyBuilder => {
+	policyBuilder.RequireAuthenticatedUser()
+	.AddAuthenticationSchemes("cookie")
+	.RequireClaim("role", "admin");
 });
 
 var app = builder.Build();
 
+// auth middlewares
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapPost("/login", async (HttpContext context, User user) => {
 
-	// check if the credentials are correct
+	// super secure authentication ;)
 	if (user is not null && user.Email == "user@domain.com" && user.Password == "12345")
 	{
 		// create the claims for the user
@@ -49,12 +60,13 @@ app.MapPost("/login", async (HttpContext context, User user) => {
 });
 
 app.MapGet("/logout", async (HttpContext context) => {
+	// deletes the cookie
 	await context.SignOutAsync("cookie");
 	return Results.Ok();
 });
 
 app.MapGet("/user", (HttpContext context) => {
-	// get the value of the claim
+	// get the auth claims from the request
 	var value = context.User.Claims.First(c => c.Type == "role").Value;
 	return Results.Ok("hello " + value);
 }).RequireAuthorization("admin");
